98e071bc22f95a23b83ed4e04fc28ab85df9ccab
sergiofdezp <sergiofdezp7@gmail.com>
2024-01-26 13:12:29 +0100

    New receta save

--- src/main/java/com/ipartek/controller/MenuController.java
+++ src/main/java/com/ipartek/controller/MenuController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ipartek.model.Dificultad;
@@ -14,34 +13,30 @@ import com.ipartek.repository.DificultadRepository;
 import com.ipartek.repository.RecetaRepository;
 
 @Controller
-public class MainController {
-	
+public class MenuController {
 	@Autowired
 	private RecetaRepository recetaRepo;
 	
 	@Autowired
 	private DificultadRepository dificultadRepo;
 	
-	@RequestMapping("/")
-	public String cargarHome(Model model) {
+	@RequestMapping("/recetas")
+	public String cargarRecetas(Model model) {
 		List<Receta> listaRecetas = recetaRepo.findAll();		
 		model.addAttribute("atr_lista_recetas", listaRecetas);
+		model.addAttribute("objeto_entidad", new Receta());
 		
 		// System.out.println(listaRecetas);
 		
 		return "index";
 	}
 	
-	@RequestMapping("/guardar_receta")
-	public String guardarReceta(@ModelAttribute Receta objeto_entidad, Model model) {
-		recetaRepo.save(objeto_entidad);
-		
+	@RequestMapping("/nueva_receta")
+	public String nuevaReceta(Model model) {
 		List<Dificultad> listaDificultades = dificultadRepo.findAll();		
-		List<Receta> listaRecetas = recetaRepo.findAll();
-		
 		model.addAttribute("atr_lista_dificultades", listaDificultades);
-		model.addAttribute("atr_lista_recetas", listaRecetas);
-
-		return "redirect:/recetas";
+		model.addAttribute("objeto_entidad", new Receta());
+		
+		return "nueva_receta";
 	}
 }
---
+++ src/main/java/com/ipartek/repository/DificultadRepository.java
@@ -0,0 +1,9 @@
+package com.ipartek.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ipartek.model.Dificultad;
+
+public interface DificultadRepository extends JpaRepository<Dificultad, Integer> {
+
+}
