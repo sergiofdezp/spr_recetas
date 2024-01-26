5e81d129fece0509889cae23691474a96c3cf158
sergiofdezp <sergiofdezp7@gmail.com>
2024-01-26 12:09:53 +0100

    RecetaRepository

---
+++ src/main/java/com/ipartek/repository/RecetaRepository.java
@@ -0,0 +1,9 @@
+package com.ipartek.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ipartek.model.Receta;
+
+public interface RecetaRepository extends JpaRepository<Receta, Integer> {
+
+}
